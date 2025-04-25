a6616773630ce08dc7bc8c44242f86f010805db3
naidenovaleksei <naidenov.aleksei@yandex.ru>
2025-04-25 22:49:25 +0200

    add distance and pytest

---
+++ tests/test_distance.py
@@ -0,0 +1,69 @@
+import pytest
+from src.envs.distance import find_path
+
+@pytest.fixture
+def sample_map():
+    # (7, 3)
+    return [
+        #0123456
+        '#######', # 0
+        '#.....#', # 1
+        '#######', # 2
+    ]
+
+@pytest.fixture
+def normal_map():
+    # (15, 14)
+    return [
+        #012345678901234
+        '###############', # 0
+        '#.............#', # 1
+        '#.#.#.#.#.#.#.#', # 2
+        '#..w.......w..#', # 3
+        '#.#.#.#.#.#.#.#', # 4
+        '#.............#', # 5
+        '#.#.#.#.#.#.#.#', # 6
+        '#.............#', # 7
+        '#.#.#.#.#.#.#.#', # 8
+        '#..w.......w..#', # 9
+        '#.#.#.#.#.#.#.#', # 0
+        '#.............#', # 1
+        '###############', # 2
+    ]
+
+@pytest.mark.parametrize("start_point, finish_point, expected", [
+    [(1, 1), (1, 1), []],
+    [(1, 1), (2, 1), [(2, 1)]],
+    [(1, 1), (4, 1), [(4, 1), (3, 1), (2, 1)]],
+])
+def test_find_path_sample(start_point, finish_point, expected, sample_map):
+    result = find_path(start_point, finish_point, sample_map)
+    # assert len(result) == len(expected)
+    assert result == expected
+
+@pytest.mark.parametrize("start_point, finish_point, expected", [
+    [(1, 1), (13, 11), [
+        (13, 11),
+        (13, 10),
+        (13, 9),
+        (13, 8),
+        (13, 7),
+        (13, 6),
+        (13, 5),
+        (13, 4),
+        (13, 3),
+        (13, 2),
+        (13, 1),(12, 1),(11, 1),(10, 1),(9, 1),(8, 1),(7, 1),(6, 1),(5, 1),(4, 1),(3, 1),(2, 1),
+    ]]
+])
+def test_find_path_normal(start_point, finish_point, expected, normal_map):
+    result = find_path(start_point, finish_point, normal_map)
+    assert len(result) == len(expected)
+    assert result == expected
+
+@pytest.mark.parametrize("start_point, finish_point", [
+    [(1, 1), (6, 1)],
+])
+def test_wrong_positions(start_point, finish_point, sample_map):
+    with pytest.raises(AssertionError):
+        find_path(start_point, finish_point, sample_map)
