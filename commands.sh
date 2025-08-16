7a6333bf8e45b586915ba917c5e00ca169c12680
naidenovaleksei <naidenov.aleksei@yandex.ru>
2025-08-16 17:24:08 +0000

    update commands

--- commands.sh
+++ commands.sh
@@ -1,8 +1,11 @@
+# venv
+source ~/kutulu-env/bin/activate
+
 # mlflow
-mlflow ui --backend-store-uri file:./mlruns --port 5001
+mlflow ui --backend-store-uri file:$HOME/projects/kutulu_artifacts/mlruns --port 5001
 
 # tensorboard
-tensorboard --logdir $HOME/projects/kutulu_artifacts/experiments/outputs
+tensorboard --logdir $HOME/projects/kutulu_artifacts/experiments/outputs --port 6007
 
 # experiment
 python experiments/run_experiment.py agent.lr=0.001 trainer.num_experiments=100 trainer.silent=false trainer.verbose=false trainer.metrics_int=10 trainer.num_envs=4 competitor=qdn_conv
